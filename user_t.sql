3d786d3de312a43518089398df671b49aa6ff526
DavisYin <48684092+Begeassgit@users.noreply.github.com>
2020-04-11 23:09:41 +0800

    Update and rename Users Information.sql to user_t.sql

---
+++ user_t.sql
@@ -0,0 +1,16 @@
+﻿USE [lib_db]
+GO
+/****** Object:  Table [dbo].[user_t]    Script Date: 2020/4/11 21:45:05 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[user_t](
+	[ReaderNo] [nvarchar](10) NOT NULL,
+	[ReaderName] [nvarchar](10) NULL,
+	[Age] [smallint] NULL,
+	[Sex] [char](2) NULL,
+	[Number] [nvarchar](20) NULL,
+	[Identity] [tinyint] NULL
+) ON [PRIMARY]
+GO
