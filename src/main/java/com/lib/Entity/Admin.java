22b52a9000c0c05baaa93de360e395dff6013709
Begeassgit <begeass@sina.com>
2020-04-15 08:49:03 +0800

    Entity Class Update

---
+++ src/main/java/com/lib/Entity/Admin.java
@@ -0,0 +1,40 @@
+package com.lib.Entity;
+
+public class Admin {
+    private String UserNo;
+    private String UserName;
+    private String Password;
+    private short Authority;
+
+    public String getUserNo() {
+        return UserNo;
+    }
+
+    public void setUserNo(String userNo) {
+        UserNo = userNo;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public short getAuthority() {
+        return Authority;
+    }
+
+    public void setAuthority(short authority) {
+        Authority = authority;
+    }
+}
---
+++ src/main/java/com/lib/Entity/User.java
@@ -0,0 +1,58 @@
+package com.lib.Entity;
+
+public class User {
+    private String ReaderNo;
+    private String ReaderName;
+    private short Age;
+    private String Sex;
+    private String Number;
+    private String Identities;
+
+    public String getReaderNo() {
+        return ReaderNo;
+    }
+
+    public void setReaderNo(String readerNo) {
+        ReaderNo = readerNo;
+    }
+
+    public String getReaderName() {
+        return ReaderName;
+    }
+
+    public void setReaderName(String readerName) {
+        ReaderName = readerName;
+    }
+
+    public short getAge() {
+        return Age;
+    }
+
+    public void setAge(short age) {
+        Age = age;
+    }
+
+    public String getSex() {
+        return Sex;
+    }
+
+    public void setSex(String sex) {
+        Sex = sex;
+    }
+
+    public String getNumber() {
+        return Number;
+    }
+
+    public void setNumber(String number) {
+        Number = number;
+    }
+
+    public String getIdentities() {
+        return Identities;
+    }
+
+    public void setIdentities(String identities) {
+        Identities = identities;
+    }
+}
