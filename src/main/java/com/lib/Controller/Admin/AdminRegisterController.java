1ed9c03f2efeaac695eb44422adb64c9d8f0f65f
Begeassgit <begeass@sina.com>
2020-04-15 19:18:25 +0800

    Login and Register Function

--- src/main/java/com/lib/Controller/Admin/AdminRegisterController.java
+++ src/main/java/com/lib/Controller/Admin/AdminRegisterController.java
@@ -4,6 +4,8 @@ import com.lib.Entity.Admin;
 import com.lib.Service.Admin.AdminAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class AdminRegisterController {
@@ -12,5 +14,16 @@ public class AdminRegisterController {
     @Autowired
     public AdminRegisterController(AdminAccountService adminAccountService){
         this.adminAccountService=adminAccountService;
+    }
+
+    @RequestMapping(value = "/Admin/Register",method = RequestMethod.GET)
+    public String registerAdminPage(){
+        return "RegisterAdmin";
+    }
+
+    @RequestMapping(value = "/Admin/Register/Submit",method = RequestMethod.POST)
+    public String registerSubmit(Admin admin){
+        adminAccountService.registerAdminService(admin);
+        return "";
     }
 }
