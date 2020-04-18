245e8111ee92c860dda2d566fb6e72610d2dd420
Begeassgit <begeass@sina.com>
2020-04-18 09:56:14 +0800

    Rest Password & UserInfoPage

--- src/main/java/com/lib/Controller/User/UserInfoController.java
+++ src/main/java/com/lib/Controller/User/UserInfoController.java
@@ -5,7 +5,6 @@ import com.lib.Service.User.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
