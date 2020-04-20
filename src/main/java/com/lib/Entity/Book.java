752632ec0fd3b057dfd83db6a4e322f02543bf1d
Begeassgit <begeass@sina.com>
2020-04-20 18:57:45 +0800

    搜索书籍

---
+++ src/main/java/com/lib/Entity/Book.java
@@ -0,0 +1,67 @@
+package com.lib.Entity;
+
+public class Book {
+    private String BookNo;
+    private String BookName;
+    private String Author;
+    private String RoomNo;
+    private String PublicInfo;
+    private String BType;
+    private int InBase;
+
+    public String getBookNo() {
+        return BookNo;
+    }
+
+    public void setBookNo(String bookNo) {
+        BookNo = bookNo;
+    }
+
+    public String getBookName() {
+        return BookName;
+    }
+
+    public void setBookName(String bookName) {
+        BookName = bookName;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public void setAuthor(String author) {
+        Author = author;
+    }
+
+    public String getRoomNo() {
+        return RoomNo;
+    }
+
+    public void setRoomNo(String roomNo) {
+        RoomNo = roomNo;
+    }
+
+    public String getPublicInfo() {
+        return PublicInfo;
+    }
+
+    public void setPublicInfo(String publicInfo) {
+        PublicInfo = publicInfo;
+    }
+
+    public String getBType() {
+        return BType;
+    }
+
+    public void setBType(String BType) {
+        this.BType = BType;
+    }
+
+    public int getInBase() {
+        return InBase;
+    }
+
+    public void setInBase(int inBase) {
+        InBase = inBase;
+    }
+}
---
+++ src/main/java/com/lib/Service/MainPage/SearchService.java
@@ -0,0 +1,7 @@
+package com.lib.Service.MainPage;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SearchService {
+}
