638062decd6f06c906e7db8c6c0b54dc41be819e
Begeassgit <begeass@sina.com>
2020-04-20 23:19:32 +0800

    搜索书籍

--- src/main/java/com/lib/Controller/User/UserLoginController.java
+++ src/main/java/com/lib/Controller/User/UserLoginController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-
 
 @Controller
 public class UserLoginController {
